0fe1974fe9c503178d423e04e74ad6524fe1d3de
Canna Wen <cannawen@gmail.com>
2023-07-21 08:31:47 -0400

    Add discord library

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # discord-moderator
+
+[Invite bot](https://discord.com/api/oauth2/authorize?client_id=1062766623578148945&permissions=8&scope=bot)
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+require("dotenv").config();
+import Discord from "discord.js";
+
+// Create a new client instance
+const client = new Discord.Client({ intents: [131071] });
+let guild: Discord.Guild | undefined;
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Discord.Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+
+  guild = client.guilds.cache.find((g) => g.id === "761903068127428649");
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.DISCORD_PRIVATE_TOKEN);
