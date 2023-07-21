062ca564e890fbb63a36609af795d3b2f78928f9
Canna Wen <cannawen@gmail.com>
2023-07-21 19:48:27 -0400

    Refactor discord user-moving code into helper file

--- src/rules/moveToChurchOfRico.ts
+++ src/rules/moveToChurchOfRico.ts
@@ -1,38 +1,20 @@
-import { Guild, VoiceChannel } from "discord.js";
+import { findVoiceChannel, foundUserInChannel, moveAllUsers } from "../helpers";
 import cron from "node-cron";
 import Rule from "../Rule";
 
-let churchOfRico: VoiceChannel;
-let dota2: VoiceChannel;
-let general: VoiceChannel;
-
-function findVoiceChannel(guild: Guild, channelId: string) {
-  return guild.channels.cache.find((c) => c.id === channelId) as VoiceChannel;
-}
-
-function foundRicoInChannel(channel: VoiceChannel) {
-  return (
-    channel.members.find((m) => m.id === process.env.USER_ID_RICO) !== undefined
-  );
-}
-
-function moveAllUsers(fromChannel: VoiceChannel, toChannel: VoiceChannel) {
-  fromChannel.members.forEach((m) => m.voice.setChannel(toChannel));
-}
-
-cron.schedule("*/2 * * * * *", () => {
-  if (foundRicoInChannel(dota2)) {
-    moveAllUsers(dota2, churchOfRico);
-  } else if (foundRicoInChannel(general)) {
-    moveAllUsers(general, churchOfRico);
-  }
-});
-
 export default new Rule((guild) => {
-  churchOfRico = findVoiceChannel(
+  const churchOfRico = findVoiceChannel(
     guild,
     process.env.CHANNEL_ID_CHURCH_OF_RICO!
   );
-  dota2 = findVoiceChannel(guild, process.env.CHANNEL_ID_DOTA_2!);
-  general = findVoiceChannel(guild, process.env.CHANNEL_ID_GENERAL!);
+  const dota2 = findVoiceChannel(guild, process.env.CHANNEL_ID_DOTA_2!);
+  const general = findVoiceChannel(guild, process.env.CHANNEL_ID_GENERAL!);
+
+  cron.schedule("*/2 * * * * *", () => {
+    if (foundUserInChannel(process.env.USER_ID_RICO!, dota2)) {
+      moveAllUsers(dota2, churchOfRico);
+    } else if (foundUserInChannel(process.env.USER_ID_RICO!, general)) {
+      moveAllUsers(general, churchOfRico);
+    }
+  });
 });
