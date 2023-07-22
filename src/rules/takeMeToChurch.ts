6b0cd38f5127818488ca9bd63d64b7b05c59bf39
Canna Wen <cannawen@gmail.com>
2023-07-22 17:58:27 -0400

    Refactor to split take me/us to church into two rules

---
+++ src/rules/takeMeToChurch.ts
@@ -0,0 +1,42 @@
+import { Guild, VoiceBasedChannel } from "discord.js";
+import constants from "../constants";
+import Rule from "../Rule";
+
+function theChurchOfRicoChannel(guild: Guild) {
+  return guild.channels.cache.find(
+    (c) => c.id === constants.channelIds.THE_CHURCH_OF_RICO
+  ) as VoiceBasedChannel;
+}
+
+export default [
+  new Rule({
+    description:
+      '"take me to church" moves users in current channel to The Church of Rico',
+    utterance: (guild, utterance) => {
+      if (utterance.match(/^take me to( church)?$/i)) {
+        const fromChannelId = guild.members.cache.find(
+          (m) => m.id === constants.userIds.CANNA
+        )?.voice.channelId;
+        const fromChannel = guild.channels.cache.find(
+          (c) => c.id === fromChannelId
+        ) as VoiceBasedChannel;
+        fromChannel.members.forEach((m) =>
+          m.voice.setChannel(theChurchOfRicoChannel(guild))
+        );
+      }
+    },
+  }),
+  new Rule({
+    description:
+      '"take us to church" moves all users connected to any voice channel to The Church of Rico',
+    utterance: (guild, utterance) => {
+      if (utterance.match(/^take us to( church)?$/i)) {
+        guild.members.cache
+          .filter((m) => m.voice.channel)
+          .forEach((m) => {
+            m.voice.setChannel(theChurchOfRicoChannel(guild));
+          });
+      }
+    },
+  }),
+];
