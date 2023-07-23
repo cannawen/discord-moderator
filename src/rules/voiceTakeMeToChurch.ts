7f88421dd214e66778934ada3544440f93259f22
Canna Wen <cannawen@gmail.com>
2023-07-22 22:27:57 -0400

    "take me to church" moves everyone from all channels [#3]

--- src/rules/voiceTakeMeToChurch.ts
+++ src/rules/voiceTakeMeToChurch.ts
@@ -9,28 +9,28 @@ function theChurchOfRicoChannel(guild: Guild) {
 }
 
 export default [
+  // new Rule({
+  //   description:
+  //     '"take me to church" moves users in current channel to The Church of Rico',
+  //   utterance: (guild, utterance) => {
+  //     if (utterance.match(/^take me to( church)?$/i)) {
+  //       const fromChannelId = guild.members.cache.find(
+  //         (m) => m.id === constants.userIds.CANNA
+  //       )?.voice.channelId;
+  //       const fromChannel = guild.channels.cache.find(
+  //         (c) => c.id === fromChannelId
+  //       ) as VoiceBasedChannel;
+  //       fromChannel.members.forEach((m) =>
+  //         m.voice.setChannel(theChurchOfRicoChannel(guild))
+  //       );
+  //     }
+  //   },
+  // }),
   new Rule({
     description:
-      '"take me to church" moves users in current channel to The Church of Rico',
+      '"take me to church" moves all users connected to any voice channel to The Church of Rico',
     utterance: (guild, utterance) => {
-      if (utterance.match(/^take me to( church)?$/i)) {
-        const fromChannelId = guild.members.cache.find(
-          (m) => m.id === constants.userIds.CANNA
-        )?.voice.channelId;
-        const fromChannel = guild.channels.cache.find(
-          (c) => c.id === fromChannelId
-        ) as VoiceBasedChannel;
-        fromChannel.members.forEach((m) =>
-          m.voice.setChannel(theChurchOfRicoChannel(guild))
-        );
-      }
-    },
-  }),
-  new Rule({
-    description:
-      '"take us to church" moves all users connected to any voice channel to The Church of Rico',
-    utterance: (guild, utterance) => {
-      if (utterance.match(/^take us to( church)?$/i)) {
+      if (utterance.match(/^take me to church$/i)) {
         guild.members.cache
           .filter((m) => m.voice.channel)
           .forEach((m) => {
