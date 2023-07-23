91b1a6503cda241b95bea51d4367c145e798ed94
Canna Wen <cannawen@gmail.com>
2023-07-22 23:23:59 -0400

    Remove unused code [#3]

--- src/rules/voiceTakeMeToChurch.ts
+++ src/rules/voiceTakeMeToChurch.ts
@@ -9,36 +9,17 @@ function theChurchOfRicoChannel(guild: Guild) {
   ) as VoiceBasedChannel;
 }
 
-export default [
-  // new Rule({
-  //   description:
-  //     '"take me to church" moves member in current channel to The Church of Rico',
-  //   utterance: (guild, utterance) => {
-  //     if (utterance.match(/^take me to( church)?$/i)) {
-  //       const fromChannelId = guild.members.cache.find(
-  //         (m) => m.id === constants.memberIds.CANNA
-  //       )?.voice.channelId;
-  //       const fromChannel = guild.channels.cache.find(
-  //         (c) => c.id === fromChannelId
-  //       ) as VoiceBasedChannel;
-  //       fromChannel.members.forEach((m) =>
-  //         m.voice.setChannel(theChurchOfRicoChannel(guild))
-  //       );
-  //     }
-  //   },
-  // }),
-  new Rule({
-    description:
-      '"take me to church" moves all members connected to any voice channel to The Church of Rico',
-    utterance: (guild, utterance) => {
-      if (utterance.match(/^take me to church$/i)) {
-        guild.members.cache
-          .filter((m) => m.voice.channel)
-          .forEach((m) => {
-            m.voice.setChannel(theChurchOfRicoChannel(guild));
-          });
-        playAudio(guild.id, "holy.mp3");
-      }
-    },
-  }),
-];
+export default new Rule({
+  description:
+    '"take me to church" moves all members connected to any voice channel to The Church of Rico',
+  utterance: (guild, utterance) => {
+    if (utterance.match(/^take me to church$/i)) {
+      guild.members.cache
+        .filter((m) => m.voice.channel)
+        .forEach((m) => {
+          m.voice.setChannel(theChurchOfRicoChannel(guild));
+        });
+      playAudio(guild.id, "holy.mp3");
+    }
+  },
+});
