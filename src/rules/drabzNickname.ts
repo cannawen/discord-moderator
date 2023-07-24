4d8fd8d2f0442f038ef3f75c6f0574a2642846e5
Canna Wen <cannawen@gmail.com>
2023-07-24 17:49:57 -0400

    Change drabz nick on tick [#6]

--- src/rules/drabzNickname.ts
+++ src/rules/drabzNickname.ts
@@ -3,19 +3,20 @@ import cron from "node-cron";
 import { findMember } from "../helpers";
 import Rule from "../Rule";
 
+const nightAnchor = new Date(2023, 6, 3, 0, 0, 0, 0);
+const afternoonAnchor = new Date(2023, 6, 17, 0, 0, 0, 0);
+const dayAnchor = new Date(2023, 6, 31, 0, 0, 0, 0);
+
 export default new Rule({
   description: "drabz",
-  start: () => {
+  tick: () => {
     const drabz = findMember(constants.memberIds.DRABZ);
-
-    cron.schedule("0 0 * * SAT", () => {
+    const day = new Date().getDay();
+    if (day === 6 || day === 0) {
       drabz.edit({ nick: "Drabz (weekend)" });
-    });
-    cron.schedule("0 0 * * MON", () => {
+    }
+    if (day !== 6 && day !== 0) {
       drabz.edit({ nick: "Drabz (weekday)" });
-    });
-    cron.schedule("0 0 * * TUE", () => {
-      drabz.edit({ nick: "Drabz (weekday)" });
-    });
+    }
   },
 });
