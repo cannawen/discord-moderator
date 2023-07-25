f1d1ea698f8d832dc93e76e9ae6d2660b50d0112
Canna Wen <cannawen@gmail.com>
2023-07-25 13:35:44 -0400

    Update drabz nick on 6-wk cycle [#6]

--- src/rules/drabzNickname.ts
+++ src/rules/drabzNickname.ts
@@ -6,9 +6,18 @@ const nightAnchor = new Date(2023, 6, 3, 0, 0, 0, 0).getTime();
 const afternoonAnchor = new Date(2023, 6, 17, 0, 0, 0, 0).getTime();
 const dayAnchor = new Date(2023, 6, 31, 0, 0, 0, 0).getTime();
 
+const sixWeeksInMs = 6 * 7 * 24 * 60 * 60 * 60 * 1000;
+
 function drabzString(date: Date) {
-  date.getTime();
-  return "Drabz (weekday)";
+  const leftovers = (date.getTime() - nightAnchor) % sixWeeksInMs;
+  if (leftovers < leftovers / 3) {
+    return "Drabz (night)";
+  }
+  if (leftovers < (2 * leftovers) / 3) {
+    return "Drabz (afternoon)";
+  }
+
+  return "Drabz (day)";
 }
 
 export default new Rule({
