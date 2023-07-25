b0b26aafb2fa193062fc307734d599b43b85daa4
Canna Wen <cannawen@gmail.com>
2023-07-24 22:44:35 -0400

    Change drabz nickname to make less network calls [#6]

--- src/rules/drabzNickname.ts
+++ src/rules/drabzNickname.ts
@@ -1,22 +1,31 @@
 import constants from "../constants";
-import cron from "node-cron";
 import { findMember } from "../helpers";
 import Rule from "../Rule";
 
-const nightAnchor = new Date(2023, 6, 3, 0, 0, 0, 0);
-const afternoonAnchor = new Date(2023, 6, 17, 0, 0, 0, 0);
-const dayAnchor = new Date(2023, 6, 31, 0, 0, 0, 0);
+const nightAnchor = new Date(2023, 6, 3, 0, 0, 0, 0).getTime();
+const afternoonAnchor = new Date(2023, 6, 17, 0, 0, 0, 0).getTime();
+const dayAnchor = new Date(2023, 6, 31, 0, 0, 0, 0).getTime();
+
+function drabzString(date: Date) {
+  date.getTime();
+  return "Drabz (weekday)";
+}
 
 export default new Rule({
   description: "drabz",
   tick: () => {
     const drabz = findMember(constants.memberIds.DRABZ);
-    const day = new Date().getDay();
-    if (day === 6 || day === 0) {
-      drabz.edit({ nick: "Drabz (weekend)" });
+    const now = new Date();
+
+    let drabzNick: string;
+    if (now.getDay() === 6 || now.getDay() === 0) {
+      drabzNick = "Drabz (weekend)";
+    } else {
+      drabzNick = drabzString(now);
     }
-    if (day !== 6 && day !== 0) {
-      drabz.edit({ nick: "Drabz (weekday)" });
+
+    if (drabz.nickname !== drabzNick) {
+      drabz.edit({ nick: drabzNick });
     }
   },
 });
