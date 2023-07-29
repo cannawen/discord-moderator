f9a715c66fc66215c6b14e5e3e5d247b42215601
Canna Wen <cannawen@gmail.com>
2023-07-29 08:19:17 -0400

    Refactor to use events instead of polling to follow Canna [#2]

--- src/rules/followCanna.ts
+++ src/rules/followCanna.ts
@@ -1,33 +1,38 @@
 import { getVoiceConnection, joinVoiceChannel } from "@discordjs/voice";
+import client from "../discordClient";
 import constants from "../constants";
+import { Events } from "discord.js";
 import { findMember } from "../helpers";
 import Rule from "../Rule";
 
 export default new Rule({
   description: "the bot joins whatever voice channel Canna is in",
-  tick: () => {
-    const canna = findMember(constants.memberIds.CANNA);
-    const cannaChannel = canna.voice.channel;
+  start: () => {
+    client.on(Events.VoiceStateUpdate, (_, voiceStatus) => {
+      if (voiceStatus.member?.id !== constants.memberIds.CANNA) return;
 
-    const bot = findMember(constants.memberIds.CANNA_BOT);
-    const botChannel = bot.voice.channel;
+      const cannaChannel = voiceStatus.channel;
 
-    // if bot is currently in a channel different from Canna
-    if (cannaChannel && botChannel !== cannaChannel) {
-      // join bot to channel
-      joinVoiceChannel({
-        adapterCreator: cannaChannel.guild.voiceAdapterCreator,
-        channelId: cannaChannel.id,
-        guildId: constants.guildIds.BEST_DOTA,
-        selfDeaf: false,
-        selfMute: false,
-      });
-    }
+      const bot = findMember(constants.memberIds.CANNA_BOT);
+      const botChannel = bot.voice.channel;
 
-    // if Canna is not in a channel but bot is in a channel
-    if (!cannaChannel && botChannel) {
-      // disconnect bot
-      getVoiceConnection(constants.guildIds.BEST_DOTA)?.destroy();
-    }
+      // if bot is currently in a channel different from Canna
+      if (cannaChannel && botChannel !== cannaChannel) {
+        // join bot to channel
+        joinVoiceChannel({
+          adapterCreator: cannaChannel.guild.voiceAdapterCreator,
+          channelId: cannaChannel.id,
+          guildId: constants.guildIds.BEST_DOTA,
+          selfDeaf: false,
+          selfMute: false,
+        });
+      }
+
+      // if Canna is not in a channel but bot is in a channel
+      if (!cannaChannel && botChannel) {
+        // disconnect bot
+        getVoiceConnection(constants.guildIds.BEST_DOTA)?.destroy();
+      }
+    });
   },
 });
