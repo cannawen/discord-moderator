cd1d53e59ea2b24db00b92c803b03077c4e30d9c
Canna Wen <cannawen@gmail.com>
2023-07-29 08:06:21 -0400

    Refactor to not hard code string

--- src/rules/addRoleOnJoin.ts
+++ src/rules/addRoleOnJoin.ts
@@ -1,11 +1,12 @@
 import client from "../discordClient";
 import constants from "../constants";
+import { Events } from "discord.js";
 import Rule from "../Rule";
 
 export default new Rule({
   description: "assign everybody-except-drabz role to new members",
   start: () => {
-    client.on("guildMemberAdd", (member) => {
+    client.on(Events.GuildMemberAdd, (member) => {
       member.roles.add(constants.roleIds.EVERYONE_EXCEPT_DRABZ);
     });
   },
