552c853ddacdef7d9ffac4c813b9329fc4d08210
Canna Wen <cannawen@gmail.com>
2023-08-01 09:04:45 -0400

    Move coach to channel when it is present

--- src/rules/bringDotaCoach.ts
+++ src/rules/bringDotaCoach.ts
@@ -1,11 +1,15 @@
 import { findMember, moveToVoiceChannel } from "../helpers";
 import constants from "../constants";
+import https from "https";
 import Rule from "../Rule";
 
 export default new Rule({
   description: "bring dota-coach to a member's voice channel",
   utterance: (utterance, memberId) => {
-    if (utterance.match(/^coach me$/i)) {
+    if (
+      utterance.match(/^coach me$/i) &&
+      findMember(constants.memberIds.DOTA_COACH).voice.channel
+    ) {
       moveToVoiceChannel(
         constants.memberIds.DOTA_COACH,
         findMember(memberId).voice.channelId!
