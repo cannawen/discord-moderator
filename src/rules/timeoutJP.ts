89f2c309a3011c70bb00835bd98cf3d253da6055
Canna Wen <cannawen@gmail.com>
2023-08-05 23:29:23 -0400

    change timeout duration to 2 minutes

--- src/rules/timeoutJP.ts
+++ src/rules/timeoutJP.ts
@@ -2,7 +2,7 @@ import constants from "../constants";
 import { findMember } from "../helpers";
 import Rule from "../Rule";
 
-const THREE_MINUTES_IN_MS = 3 * 60 * 1000;
+const TIMEOUT_DURATION_IN_MS = 2 * 60 * 1000;
 
 export default new Rule({
   description: "voice command to time out jproperly for 3 minutes",
@@ -16,7 +16,7 @@ export default new Rule({
         jproperly.edit({ mute: true });
         setTimeout(() => {
           jproperly.edit({ mute: false });
-        }, THREE_MINUTES_IN_MS);
+        }, TIMEOUT_DURATION_IN_MS);
       }
     } catch (_) {}
   },
