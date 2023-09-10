940ff168b682273b02494df61be8f2fcaa546981
Canna Wen <cannawen@gmail.com>
2023-09-10 10:22:25 -0400

    Fix crash when JP is not connected to voice channel [#8]

--- src/rules/timeoutJP.ts
+++ src/rules/timeoutJP.ts
@@ -7,17 +7,18 @@ const TIMEOUT_DURATION_IN_MS = 2 * 60 * 1000;
 export default new Rule({
   description: "voice command to time out jproperly for 2 minutes",
   utterance: (utterance, memberId) => {
-    try {
-      if (
-        utterance.match(/^mute jp$/i) &&
-        memberId === constants.memberIds.CANNA
-      ) {
-        const jproperly = findMember(constants.memberIds.JPROPERLY);
-        jproperly.edit({ mute: true });
-        setTimeout(() => {
-          jproperly.edit({ mute: false });
-        }, TIMEOUT_DURATION_IN_MS);
+    if (
+      utterance.match(/^mute jp$/i) &&
+      memberId === constants.memberIds.CANNA
+    ) {
+      const jproperly = findMember(constants.memberIds.JPROPERLY);
+      if (jproperly.voice.channel === null) {
+        return;
       }
-    } catch (_) {}
+      jproperly.edit({ mute: true });
+      setTimeout(() => {
+        jproperly.edit({ mute: false });
+      }, TIMEOUT_DURATION_IN_MS);
+    }
   },
 });
