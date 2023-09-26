34a2a04ba3de1d040f07291445b49676cc763c47
Canna Wen <cannawen@gmail.com>
2023-09-26 17:39:38 -0400

    Fix `/app/start.sh: line 5: /app/my-app: not found`

--- start.sh
+++ start.sh
@@ -2,4 +2,3 @@
 
 /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=fly-app
-/app/my-app
