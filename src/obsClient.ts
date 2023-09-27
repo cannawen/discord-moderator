cf771fc234a6a9f9e05bfb407737426c8dcbb95b
Canna Wen <cannawen@gmail.com>
2023-09-27 13:12:21 -0400

    Start and stop OBS replay buffer depending on if I'm on Discord

--- src/obsClient.ts
+++ src/obsClient.ts
@@ -5,6 +5,13 @@ let obs = new OBSWebSocket();
 function connect() {
   return obs
     .connect(`ws://${constants.obs.SERVER}:4455`, constants.obs.SERVER_PASSWORD)
+    .then(() =>
+      obs.call("GetReplayBufferStatus").then((response) => {
+        if (!response.outputActive) {
+          obs.call("StartReplayBuffer");
+        }
+      })
+    )
     .catch((e) => {
       console.log("failed to connect to OBS");
       throw e;
@@ -12,7 +19,7 @@ function connect() {
 }
 
 function disconnect() {
-  return obs.disconnect();
+  return obs.call("StopReplayBuffer").finally(() => obs.disconnect());
 }
 
 function clip() {
