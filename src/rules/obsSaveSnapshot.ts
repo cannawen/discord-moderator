9d61f23e37d0bfd9826dc6d9346fec3a9f3674ab
Canna Wen <cannawen@gmail.com>
2023-10-08 11:35:02 -0400

    Provide immediate feedback after snapshot command

--- src/rules/obsSaveSnapshot.ts
+++ src/rules/obsSaveSnapshot.ts
@@ -6,11 +6,9 @@ export default new Rule({
   description: "when someone says 'snapshot' record OBS stream",
   utterance: (utterance) => {
     if (utterance.match(/^(snapshot|Snapchat)$/i)) {
+      playAudio("photo.mp3");
       setTimeout(() => {
-        obsClient
-          .clip()
-          .then(() => playAudio("photo.mp3"))
-          .catch(() => playAudio("error.mp3"));
+        obsClient.clip().catch(() => playAudio("error.mp3"));
       }, 2 * 1000);
     }
   },
