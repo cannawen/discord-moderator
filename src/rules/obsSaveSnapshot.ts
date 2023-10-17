f7ee1796ccfefffb62a76e9ea8204d1cd1210373
Canna Wen <cannawen@gmail.com>
2023-10-16 23:59:02 -0400

    Save 5 seconds after snapshot command

--- src/rules/obsSaveSnapshot.ts
+++ src/rules/obsSaveSnapshot.ts
@@ -9,7 +9,7 @@ export default new Rule({
       playAudio("photo.mp3");
       setTimeout(() => {
         obsClient.clip().catch(() => playAudio("error.mp3"));
-      }, 2 * 1000);
+      }, 5 * 1000);
     }
   },
 });
