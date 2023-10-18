ad54633c095de61d87d09f297603605388a4ad55
Canna Wen <cannawen@gmail.com>
2023-10-17 23:33:56 -0400

    Revert "Connect to stream OBS server only (no need for game OBS server)"
    This reverts commit 9b270dc67d7a5a9a57f98fe099bde32ac6b3e36c.

--- src/obsClient.ts
+++ src/obsClient.ts
@@ -2,46 +2,61 @@ import OBSWebSocket from "obs-websocket-js";
 import constants from "./constants";
 
 let obsStream = new OBSWebSocket();
+let obsGame = new OBSWebSocket();
 
 function connect() {
-  return obsStream
-    .connect(
-      `ws://${constants.obs.CANNA_STREAM_SERVER}:4455`,
-      constants.obs.CANNA_STREAM_SERVER_PASSWORD
-    )
-    .then(() =>
-      obsStream
-        .call("GetReplayBufferStatus")
-        .then((response) => {
-          if (!response.outputActive) {
-            return obsStream.call("StartReplayBuffer");
-          }
-        })
-        .catch((e) => {
-          console.log("failed to start replay buffer");
-          throw e;
-        })
-    )
-    .catch((e) => {
-      console.log("failed to connect to stream OBS");
-      throw e;
-    });
+  return Promise.all([
+    obsGame
+      .connect(
+        `ws://${constants.obs.CANNA_GAME_SERVER}:4455`,
+        constants.obs.CANNA_GAME_SERVER_PASSWORD
+      )
+      .catch((e) => {
+        console.log("failed to connect to game OBS");
+        throw e;
+      }),
+    obsStream
+      .connect(
+        `ws://${constants.obs.CANNA_STREAM_SERVER}:4455`,
+        constants.obs.CANNA_STREAM_SERVER_PASSWORD
+      )
+      .then(() =>
+        obsStream
+          .call("GetReplayBufferStatus")
+          .then((response) => {
+            if (!response.outputActive) {
+              return obsStream.call("StartReplayBuffer");
+            }
+          })
+          .catch((e) => {
+            console.log("failed to start replay buffer");
+            throw e;
+          })
+      )
+      .catch((e) => {
+        console.log("failed to connect to stream OBS");
+        throw e;
+      }),
+  ]);
 }
 
 function disconnect() {
-  return obsStream
-    .call("StopReplayBuffer")
-    .finally(() => obsStream.disconnect())
-    .catch(() => {
-      console.log("failed to stop replay buffer or disconnect from OBS");
-    });
+  Promise.all([
+    obsGame.disconnect(),
+    obsStream.call("StopReplayBuffer").finally(() => obsStream.disconnect()),
+  ]).catch(() => {
+    console.log("failed to stop replay buffer or disconnect from OBS");
+  });
 }
 
 function clip() {
-  return obsStream.call("SaveReplayBuffer").catch((e) => {
-    console.log("failed to save replay buffer");
-    throw e;
-  });
+  return Promise.all([
+    obsGame.reidentify({}),
+    obsStream.call("SaveReplayBuffer").catch((e) => {
+      console.log("failed to save replay buffer");
+      throw e;
+    }),
+  ]);
 }
 
 export default {
