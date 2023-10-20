c00555363d30a8782393f82ee19ff51761a22460
Canna Wen <cannawen@gmail.com>
2023-10-20 11:24:11 -0400

    `disconnect` voice command for when `see you later nerds` doesn't work

--- src/rules/disconnectVoiceCommand.ts
+++ src/rules/disconnectVoiceCommand.ts
@@ -7,7 +7,10 @@ export default new Rule({
   description:
     'disconnects user on "see you later nerds" and stops coaching when Canna says it',
   utterance: (utterance, memberId) => {
-    if (utterance.match(/^see you later nerds?$/i)) {
+    if (
+      utterance.match(/^see you later nerds?$/i) ||
+      utterance.match(/^disconnect$/i)
+    ) {
       findMember(memberId).voice.disconnect();
 
       if (memberId === constants.memberIds.CANNA) {
