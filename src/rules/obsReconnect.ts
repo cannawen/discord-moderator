d928f25d02e657128fec6c711931d2d1482fd5e3
tnnguyen13 <nguyeti9@oregonstate.edu>
2023-10-21 09:51:45 -0700

    Adds functionality for 'reconnecting' both Canna's and Teazy's OBS instances.

--- src/rules/obsReconnect.ts
+++ src/rules/obsReconnect.ts
@@ -1,16 +1,25 @@
 import Rule from "../Rule";
+import constants from "../constants";
 import { playAudio } from "../helpers";
 import obsClient from "../obsClient";
 
 export default new Rule({
   description:
     "Reconnect voice command triggers bot to re-initialize obs client",
-  utterance: (utterance) => {
+  utterance: (utterance, memberId) => {
     if (utterance.match(/^reconnect$/i)) {
-      obsClient
-        .connectCanna()
-        .then(() => playAudio("success.mp3"))
-        .catch(() => playAudio("error.mp3"));
+      if (memberId === constants.memberIds.CANNA) {
+        obsClient
+          .connectCanna()
+          .then(() => playAudio("success.mp3"))
+          .catch(() => playAudio("error.mp3"));
+      }
+      if (memberId === constants.memberIds.TEAZY) {
+        obsClient
+          .connectTeazy()
+          .then(() => playAudio("success.mp3"))
+          .catch(() => playAudio("error.mp3"));
+      }
     }
   },
 });
