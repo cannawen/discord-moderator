b51c5ab0323fe1d3c79df9429c23e112c936cde0
Canna Wen <cannawen@gmail.com>
2023-10-20 23:33:34 -0400

    Remove early return

--- src/rules/connectBotToVoiceChannel.ts
+++ src/rules/connectBotToVoiceChannel.ts
@@ -29,33 +29,34 @@ export default new Rule({
 
     client.on(Events.VoiceStateUpdate, (_, voiceStatus) => {
       if (
-        voiceStatus.member?.id !== constants.memberIds.CANNA &&
-        voiceStatus.member?.id !== constants.memberIds.TEAZY
-      )
-        return;
+        voiceStatus.member?.id === constants.memberIds.CANNA ||
+        voiceStatus.member?.id === constants.memberIds.TEAZY
+      ) {
+        const botChannel = findMember(constants.memberIds.CANNA_BOT).voice
+          .channel;
+        const cannaChannel = findMember(constants.memberIds.CANNA).voice
+          .channel;
+        const teazyChannel = findMember(constants.memberIds.TEAZY).voice
+          .channel;
 
-      const botChannel = findMember(constants.memberIds.CANNA_BOT).voice
-        .channel;
-      const cannaChannel = findMember(constants.memberIds.CANNA).voice.channel;
-      const teazyChannel = findMember(constants.memberIds.TEAZY).voice.channel;
+        // if bot is not in Canna's channel
+        if (cannaChannel && botChannel !== cannaChannel) {
+          // join bot to Canna channel
+          joinChannel(cannaChannel);
+          obsClient.connect().catch(() => playAudio("error.mp3"));
+        }
+        // if Canna is not connected, and the bot is not in Teazy's channel
+        if (!cannaChannel && teazyChannel && botChannel !== teazyChannel) {
+          // connect bot to Teazy's channel
+          joinChannel(teazyChannel);
+        }
 
-      // if bot is not in Canna's channel
-      if (cannaChannel && botChannel !== cannaChannel) {
-        // join bot to Canna channel
-        joinChannel(cannaChannel);
-        obsClient.connect().catch(() => playAudio("error.mp3"));
-      }
-      // if Canna is not connected, and the bot is not in Teazy's channel
-      if (!cannaChannel && teazyChannel && botChannel !== teazyChannel) {
-        // connect bot to Teazy's channel
-        joinChannel(teazyChannel);
-      }
-
-      // if Canna and Teazy are disconnected but the bot is still connected
-      if (!cannaChannel && !teazyChannel && botChannel) {
-        // disconnect bot
-        getVoiceConnection(constants.guildIds.BEST_DOTA)?.destroy();
-        obsClient.disconnect();
+        // if Canna and Teazy are disconnected but the bot is still connected
+        if (!cannaChannel && !teazyChannel && botChannel) {
+          // disconnect bot
+          getVoiceConnection(constants.guildIds.BEST_DOTA)?.destroy();
+          obsClient.disconnect();
+        }
       }
     });
   },
