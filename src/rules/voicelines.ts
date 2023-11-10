ddccbf8ce8d642c3f64f9b5395f461c8751e6d3e
Canna Wen <cannawen@gmail.com>
2023-11-10 16:30:52 -0500

    Refactor voiceline to handle randomly selecting an audio file from array

--- src/rules/voicelines.ts
+++ src/rules/voicelines.ts
@@ -1,36 +1,27 @@
 import { playAudio } from "../helpers";
 import Rule from "../Rule";
 
-// [file name, regex that triggers the voiceline (optional - if different than file name)]
+function chooseRandomFromArray(array: string[]) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+// ["file name" or ["array", "of file names"], "regex that triggers the voiceline (optional - if different than file name)"]
 export default [
-  ["slayTogether", "(play|win|lose|blues|slay|when) together"],
+  ["slayTogether", "(play|win|lose|blues|slay|when)? ?together"],
   ["faster"],
   ["player"],
-]
-  .map(
-    ([fileName, regexString]) =>
-      new Rule({
-        description: `${fileName} voiceline`,
-        utterance: (utterance) => {
-          if (
-            utterance.match(new RegExp(`^${regexString || fileName}$`, "i"))
-          ) {
-            playAudio(`voicelines/${fileName}.mp3`);
-          }
-        },
-      })
-  )
-  .concat(
+  [["egg-canna", "egg-spearit"], "egg"],
+].map(
+  ([fileNameOrNames, regexString]) =>
     new Rule({
-      description: "egg voiceline",
+      description: `${regexString || fileNameOrNames} voiceline`,
       utterance: (utterance) => {
-        if (utterance.match(/^egg$/i)) {
-          if (Math.random() < 0.5) {
-            playAudio("voicelines/egg-canna.mp3");
-          } else {
-            playAudio("voicelines/egg-spearit.mp3");
-          }
+        let fileName = Array.isArray(fileNameOrNames)
+          ? chooseRandomFromArray(fileNameOrNames)
+          : fileNameOrNames;
+        if (utterance.match(new RegExp(`^${regexString || fileName}$`, "i"))) {
+          playAudio(`voicelines/${fileName}.mp3`);
         }
       },
     })
-  );
+);
