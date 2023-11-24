4db92a91d2026e654f8a811c2c93b735c4d69908
Canna Wen <cannawen@gmail.com>
2023-11-24 08:03:37 -0500

    Manage channel names on tick instead of on voice status change

---
+++ src/rules/manageVoiceChannelNames.ts
@@ -0,0 +1,52 @@
+import { findMemberVoiceChannelId, findVoiceChannel } from "../helpers";
+import constants from "../constants";
+import Rule from "../Rule";
+
+function wrabbitChannel() {
+  return findMemberVoiceChannelId(constants.memberIds.WRABBIT);
+}
+
+function wrabbitAndTargetInSameChannel() {
+  return (
+    wrabbitChannel() !== null &&
+    wrabbitChannel() === findMemberVoiceChannelId(constants.memberIds.TARGET)
+  );
+}
+
+function updateChannelName(channelId: string, newName: string) {
+  const channel = findVoiceChannel(channelId);
+  if (channel.name !== newName) {
+    channel.setName(newName);
+  }
+}
+
+export default new Rule({
+  description:
+    "When wrabbit and target are together in a channel, rename it Home",
+  tick: () => {
+    if (wrabbitAndTargetInSameChannel()) {
+      updateChannelName(wrabbitChannel()!, constants.channelNames.HOME);
+    } else {
+      updateChannelName(
+        constants.channelIds.GENERAL,
+        constants.channelNames.GENERAL
+      );
+      updateChannelName(
+        constants.channelIds.DOTA_2,
+        constants.channelNames.DOTA_2
+      );
+      updateChannelName(
+        constants.channelIds.SECRETS,
+        constants.channelNames.SECRETS
+      );
+      updateChannelName(
+        constants.channelIds.REAL_SECRETS,
+        constants.channelNames.REAL_SECRETS
+      );
+      updateChannelName(
+        constants.channelIds.TIMEOUT,
+        constants.channelNames.TIMEOUT
+      );
+    }
+  },
+});
