f3b1f36264fbc0284750732dd873530a62e74671
Canna Wen <cannawen@gmail.com>
2024-01-11 12:38:00 -0500

    Implement being able to ask the bot a question in one utterance [#24]

--- src/rules/voiceCommands/askAiAnything.ts
+++ src/rules/voiceCommands/askAiAnything.ts
@@ -8,35 +8,51 @@ let questioningMember: string | undefined;
 
 const openai = new OpenAi({ apiKey: constants.openAi.CHATGPT_SECRET_KEY });
 
+function handleQuestion(question: string) {
+  winston.info(`Question - ${question}`);
+  openai.chat.completions
+    .create({
+      messages: [{ role: "user", content: question }],
+      model: "gpt-3.5-turbo",
+    })
+    .then((completion) => {
+      const answer = completion.choices[0].message.content;
+      winston.info(`Answer - ${answer}`);
+      playAudio(answer ? answer : "error.mp3");
+    })
+    .catch((e) => {
+      playAudio("error.mp3");
+      winston.error("Unable to answer question");
+      winston.error(e);
+    });
+}
+
 export default new Rule({
-  description: "listen for keyword",
+  description: "listen for question and responds with answer",
   utterance: (utterance, memberId) => {
-    if (
-      !questioningMember &&
-      utterance.match(/^(okay|ok) (bot|but|bought)$/i)
-    ) {
-      questioningMember = memberId;
-      playAudio("success.mp3");
+    const triggerMatch = utterance.match(
+      /^(okay|ok|hey|hay) (bot|but|bought)(.+)$/i
+    );
+
+    if (triggerMatch) {
+      if (triggerMatch[3]) {
+        handleQuestion(triggerMatch[3]);
+      } else {
+        // limitation:
+        // if two people say `OK Bot` one right after the other
+        // the bot will only answer the second person
+        questioningMember = memberId;
+        playAudio("success.mp3");
+      }
       return;
     }
 
+    // ideally this would be two rules so we don't need the early return
+    // but there is no guarentee what order the rules will be run in
+    // and it would be bad to have some weird race conditions since we
+    // are using a global variable to convey information (kinda sus)
     if (questioningMember === memberId) {
-      winston.info(`Question - ${utterance}`);
-      openai.chat.completions
-        .create({
-          messages: [{ role: "user", content: utterance }],
-          model: "gpt-3.5-turbo",
-        })
-        .then((completion) => {
-          const answer = completion.choices[0].message.content;
-          winston.info(`Answer - ${answer}`);
-          playAudio(answer ? answer : "error.mp3");
-        })
-        .catch((e) => {
-          playAudio("error.mp3");
-          winston.error("Unable to answer question");
-          winston.error(e);
-        });
+      handleQuestion(utterance);
       questioningMember = undefined;
       return;
     }
