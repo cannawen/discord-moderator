aa2ae7cd3a3a32b105076d0350be5dd938f86351
Canna Wen <cannawen@gmail.com>
2024-01-31 00:44:06 -0500

    Refactor to use Sound class to represent data for soundboard clips

--- src/rules/voiceCommands/voicelines.ts
+++ src/rules/voiceCommands/voicelines.ts
@@ -6,6 +6,39 @@ function chooseRandomFromArray(array: string[]) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
+class Sound {
+  private fileNameOrNamesArray: string | string[];
+  public regex: RegExp;
+
+  constructor(
+    fileName: string | string[],
+    regexString?: string,
+    trigger: boolean = true
+  ) {
+    this.fileNameOrNamesArray = fileName;
+    this.regex = new RegExp(
+      `^(${trigger ? TRIGGER_PHRASES : ""}) ?(${regexString || fileName})$`,
+      "i"
+    );
+  }
+
+  description(): string {
+    return `${
+      Array.isArray(this.fileNameOrNamesArray)
+        ? this.fileNameOrNamesArray.join("/")
+        : this.fileNameOrNamesArray
+    } voiceline`;
+  }
+
+  fileName(): string {
+    if (Array.isArray(this.fileNameOrNamesArray)) {
+      return chooseRandomFromArray(this.fileNameOrNamesArray);
+    } else {
+      return this.fileNameOrNamesArray;
+    }
+  }
+}
+
 const TRIGGER_PHRASES = [
   "sound ?board",
   "voice ?lines?",
@@ -16,46 +49,35 @@ const TRIGGER_PHRASES = [
 
 // ["file name" or ["array", "of file names"], "regex that triggers the voiceline (optional - if different than file name)"]
 export default [
-  ["slayTogether", "(play|win|lose|blues|slay|when)? ?together"],
-  ["faster"],
-  ["player"],
-  [["egg-canna", "egg-spearit"], "eggs?"],
-  ["egg-canna", "eggs? 1"],
-  ["egg-spearit", "eggs? 2"],
-  ["bash", "bash|bosch|back"],
-  ["pretty"],
-  [["hk-canna", "hk-crash"], "hk"],
-  ["hk-canna", "hk 1"],
-  ["hk-crash", "hk 2"],
-  ["highground", "high ?ground"],
-  ["despacito", "(that|this) is so sad"],
+  new Sound("slayTogether", "(play|win|lose|blues|slay|when)? ?together"),
+  new Sound("faster"),
+  new Sound("player"),
+  new Sound(["egg-canna", "egg-spearit"], "eggs?"),
+  new Sound("egg-canna", "eggs? 1"),
+  new Sound("egg-spearit", "eggs? 2"),
+  new Sound("bash", "bash|bosch|back"),
+  new Sound("pretty"),
+  new Sound(["hk-canna", "hk-crash"], "hk"),
+  new Sound("hk-canna", "hk 1"),
+  new Sound("hk-crash", "hk 2"),
+  new Sound("highground", "high ?ground"),
+  new Sound(
+    "despacito",
+    "((that is|this is|that's) so sad)|(Alexa play despacito)|((that is|this is|that's) so sad Alexa play despacito)",
+    false
+  ),
 ].map(
-  ([fileNameOrNames, regexString]) =>
+  (sound) =>
     new Rule({
-      description: `${
-        Array.isArray(fileNameOrNames)
-          ? fileNameOrNames.join("/")
-          : fileNameOrNames
-      } voiceline`,
+      description: sound.description(),
       utterance: (utterance, memberId) => {
-        const fileName = Array.isArray(fileNameOrNames)
-          ? chooseRandomFromArray(fileNameOrNames)
-          : fileNameOrNames;
-
-        if (
-          utterance.match(
-            new RegExp(
-              `^(${TRIGGER_PHRASES}) (${regexString || fileName})$`,
-              "i"
-            )
-          )
-        ) {
+        if (utterance.match(sound.regex)) {
           winston.info(
             `Audio - voiceline triggered with phrase "${utterance}" (${
               findMember(memberId).displayName
             })`
           );
-          playAudio(`voicelines/${fileName}.mp3`);
+          playAudio(`voicelines/${sound.fileName()}.mp3`);
         }
       },
     })
