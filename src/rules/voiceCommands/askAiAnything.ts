25a1e8dc193e56dffc2a9e7731d45ca5f5761129
Canna Wen <cannawen@gmail.com>
2024-03-13 01:20:35 -0400

    Add dota 2 poetry creation feature

--- src/rules/voiceCommands/askAiAnything.ts
+++ src/rules/voiceCommands/askAiAnything.ts
@@ -4,76 +4,48 @@ import OpenAi from "openai";
 import Rule from "../../Rule";
 import winston from "winston";
 
-enum BotPersonality {
-  DadJoke,
-  Helpful,
-}
-
-const BotPersonalityToBotSystemInstruction = new Map<BotPersonality, string>([
-  [
-    BotPersonality.DadJoke,
-    "You are a funny assistant who answers questions in one short sentence. Respond with puns when possible.",
-  ],
-  [
-    BotPersonality.Helpful,
-    "You are a helpful assistant who answers questions in one short sentence.",
-  ],
-]);
-
-let state: { [key: string]: BotPersonality } = {};
-
 const openAi = new OpenAi({ apiKey: constants.openAi.CHATGPT_SECRET_KEY });
 
-function handleQuestion(question: string, memberId: string) {
-  if (question.length < 10) return;
-  winston.info(`Question - ${question} (${findMember(memberId).displayName})`);
-  openAi.chat.completions
-    .create({
-      messages: [
-        {
-          role: "system",
-          content:
-            BotPersonalityToBotSystemInstruction.get(state[memberId]) || "",
-        },
-        { role: "user", content: question },
-      ],
-      model: "gpt-3.5-turbo",
-    })
-    .then((completion) => {
-      const answer = completion.choices[0].message.content;
-      playAudio(answer ? answer : "error.mp3");
-    })
-    .catch((e) => {
-      playAudio("error.mp3");
-      winston.error(e);
-    });
-  delete state[memberId];
+class Personality {
+  systemInstruction: string;
+  regexKeyword: string;
+  regex: RegExp;
+
+  constructor(systemInstruction: string, regexKeyword: string) {
+    this.systemInstruction = systemInstruction;
+    this.regexKeyword = regexKeyword;
+    this.regex = new RegExp(`(okay|ok|hey|hay) (${this.regexKeyword})`, "i");
+  }
+
+  handleQuestion(question: string): Promise<string> {
+    return openAi.chat.completions
+      .create({
+        messages: [
+          {
+            role: "system",
+            content: this.systemInstruction,
+          },
+          { role: "user", content: question },
+        ],
+        model: "gpt-4",
+      })
+      .then((completion) => {
+        const response = completion.choices[0].message.content;
+        if (response) {
+          return response;
+        } else {
+          throw "Did not recieve response";
+        }
+      });
+  }
 }
 
+let state: { [key: string]: Personality } = {};
+
+// Rules run in imperative order (top to bottom)
+// it is very important the cancel, then answer, then question are in this order
+// for the state to be parsed properly (this is kinda sketchy)
 export default [
-  new Rule({
-    description: "listen for question and responds with answer",
-    utterance: (utterance, memberId) => {
-      if (memberId in state) {
-        handleQuestion(utterance, memberId);
-      } else {
-        const triggerMatch = utterance.match(
-          /^(okay|ok|hey|hay) (?<personality>bot|but|bought|dad)$/i
-        );
-
-        if (triggerMatch) {
-          const dadJoke =
-            triggerMatch.groups?.personality.match(/dad/i) !== null;
-          state[memberId] = dadJoke
-            ? BotPersonality.DadJoke
-            : BotPersonality.Helpful;
-
-          playAudio("success.mp3");
-          winston.info(`Question - Trigger - ${utterance}`);
-        }
-      }
-    },
-  }),
   new Rule({
     description: "cancel asking question",
     utterance: (utterance) => {
@@ -82,4 +54,55 @@ export default [
       }
     },
   }),
-];
+  new Rule({
+    description: "ask AI question",
+    utterance: (utterance, memberId) => {
+      if (memberId in state) {
+        state[memberId]
+          .handleQuestion(utterance)
+          .then((answer) => {
+            playAudio(answer);
+            winston.info(
+              `Question - ${utterance} (${findMember(memberId).displayName})`
+            );
+          })
+          .catch((e) => {
+            playAudio("error.mp3");
+            winston.error(e);
+          });
+        delete state[memberId];
+      }
+    },
+  }),
+].concat(
+  [
+    new Personality(
+      "You are an assistant who creates haikus about Dota 2",
+      "haiku"
+    ),
+    new Personality(
+      "You are an assistant who creates limericks about Dota 2",
+      "limerick|poem"
+    ),
+    new Personality(
+      "You are a funny assistant who answers questions in one short sentence. Respond with puns when possible.",
+      "dad"
+    ),
+    new Personality(
+      "You are a helpful assistant who answers questions in one short sentence.",
+      "bot|but|bought"
+    ),
+  ].map(
+    (personality) =>
+      new Rule({
+        description: `${personality.regexKeyword} parse that you are asking AI something`,
+        utterance: (utterance, memberId) => {
+          if (utterance.match(personality.regex)) {
+            state[memberId] = personality;
+            playAudio("success.mp3");
+            winston.info(`Question - Trigger - ${utterance}`);
+          }
+        },
+      })
+  )
+);
