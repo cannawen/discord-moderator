1ed2abc6693f0b2b2d47d655481157515677947c
Canna Wen <cannawen@gmail.com>
2024-05-23 09:12:50 -0400

    Update Drabz nickname to remove shift information

--- src/rules/drabzNickname.ts
+++ src/rules/drabzNickname.ts
@@ -5,9 +5,6 @@ import Holidays from "date-holidays";
 import Rule from "../Rule";
 import winston from "winston";
 
-const NIGHT_ANCHOR = new Date(2023, 10, 20, 0, 0, 0, 0);
-const SIX_WEEKS_IN_MS = 6 * 7 * 24 * 60 * 60 * 1000;
-
 function isWeekend(date: Date) {
   return date.getDay() === 6 || date.getDay() === 0;
 }
@@ -23,28 +20,6 @@ function isHoliday(date: Date) {
   return false;
 }
 
-function percentageInto6WeekCycle(date: Date) {
-  return (
-    ((date.getTime() - NIGHT_ANCHOR.getTime()) % SIX_WEEKS_IN_MS) /
-    SIX_WEEKS_IN_MS
-  );
-}
-
-function isNightShift(date: Date) {
-  return percentageInto6WeekCycle(date) < 1 / 3;
-}
-
-function isAfternoonShift(date: Date) {
-  return (
-    percentageInto6WeekCycle(date) >= 1 / 3 &&
-    percentageInto6WeekCycle(date) < 2 / 3
-  );
-}
-
-function isDayShift(date: Date) {
-  return percentageInto6WeekCycle(date) >= 2 / 3;
-}
-
 function drabzNicknameString(date: Date) {
   if (isWeekend(date)) {
     return "Drabz (weekend)";
@@ -52,21 +27,11 @@ function drabzNicknameString(date: Date) {
   if (isHoliday(date)) {
     return "Drabz (holiday)";
   }
-  if (isNightShift(date)) {
-    return "Drabz (night)";
-  }
-  if (isAfternoonShift(date)) {
-    return "Drabz (afternoon)";
-  }
-  if (isDayShift(date)) {
-    return "Drabz (day)";
-  }
-  return "Drabz";
+  return "Drabz (weekday)";
 }
 
 export default new Rule({
-  description:
-    "edit drabz nickname to reflect his work schedule (night-afternoon-day 6 week cycle anchored November 20rd, 2023)",
+  description: "edit drabz nickname to reflect weekday/weekend/holiday",
   start: () => {
     cron.schedule("0 0 * * *", () => {
       const today = new Date();
