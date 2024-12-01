f90b209fb6beec3ef496d41ea3db12b720c90553
Canna Wen <cannawen@gmail.com>
2024-12-01 15:50:36 -0500

    Log sigint/sigterm signals

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import logger from "./logger";
 import { initDiscord } from "./discordClient";
+import winston from "winston";
 
 logger.setup();
 initDiscord();
+
+function handleShutdown() {
+    winston.error("App shutdown signal received");
+}
+
+process.on("SIGINT", handleShutdown);
+process.on("SIGTERM", handleShutdown);
