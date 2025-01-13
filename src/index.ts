d821b8d44714bfc32cc8e173990496a302000b1e
Canna Wen <cannawen@gmail.com>
2025-01-13 15:38:18 -0500

    Remove log when shutting down app

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
 import logger from "./logger";
 import { initDiscord } from "./discordClient";
-import winston from "winston";
 
 logger.setup();
 initDiscord();
-
-function handleShutdown() {
-    winston.error("App shutdown signal received");
-}
-
-process.on("SIGINT", handleShutdown);
-process.on("SIGTERM", handleShutdown);
