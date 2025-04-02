dd5fbc7273bfbd9f0603c55e49e137fa0b579220
Canna Wen <cannawen@gmail.com>
2025-04-02 01:01:47 -0400

    Disconnect bot from discord when app is killed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-import logger from "./logger";
+import constants from "./constants";
+import { findMember } from "./helpers";
 import { initDiscord } from "./discordClient";
+import logger from "./logger";
+import winston from "winston";
 
 logger.setup();
 initDiscord();
+
+function handleShutdown() {
+    winston.info("Received shutdown signal, shutting down gracefully...");
+    findMember(constants.discord.memberIds.CANNA_BOT).voice.disconnect().then(() => {
+        process.exit(0);
+    });
+}
+
+process.on("SIGINT", handleShutdown);
+process.on("SIGTERM", handleShutdown);
